ad8be002a153dcac7f78f9e7a074f7595f631b64
athithyanac <100773974+athithyanac@users.noreply.github.com>
2022-04-17 19:21:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
+#--------------------------------------
 #creating vpc
+#--------------------------------------
 resource "aws_vpc" "vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
@@ -10,7 +12,9 @@ resource "aws_vpc" "vpc" {
     project = var.project
   }
 }
+#--------------------------------------
 #creating public subnets
+#--------------------------------------
 resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = cidrsubnet(var.vpc_cidr, var.newbits, 0)
@@ -44,7 +48,9 @@ resource "aws_subnet" "public3" {
     project = var.project
   }
 }
+#--------------------------------------
 #creating igw
+#--------------------------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
@@ -66,7 +72,9 @@ resource "aws_route_table" "public_rtb" {
     project = var.project
   }
 }
+#--------------------------------------
 #creating public rtb association
+#--------------------------------------
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.public_rtb.id
@@ -79,7 +87,9 @@ resource "aws_route_table_association" "public3" {
   subnet_id      = aws_subnet.public3.id
   route_table_id = aws_route_table.public_rtb.id
 }
+#--------------------------------------
 #creating private subnets
+#--------------------------------------
 resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = cidrsubnet(var.vpc_cidr, var.newbits, 3)
@@ -107,7 +117,9 @@ resource "aws_subnet" "private3" {
     project = var.project
   }
 }
+#--------------------------------------
 #creating private rtb
+#--------------------------------------
 resource "aws_route_table" "private_rtb" {
   vpc_id = aws_vpc.vpc.id
 
@@ -120,7 +132,9 @@ resource "aws_route_table" "private_rtb" {
     project = var.project
   }
 }
+#--------------------------------------
 #creating private rtb association
+#--------------------------------------
 resource "aws_route_table_association" "private1" {
   subnet_id      = aws_subnet.private1.id
   route_table_id = aws_route_table.private_rtb.id
@@ -133,7 +147,9 @@ resource "aws_route_table_association" "private3" {
   subnet_id      = aws_subnet.private3.id
   route_table_id = aws_route_table.private_rtb.id
 }
+#--------------------------------------
 #creating nat-gw
+#--------------------------------------
 resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public1.id
@@ -144,7 +160,9 @@ resource "aws_nat_gateway" "natgw" {
   }
   depends_on = [aws_internet_gateway.igw]
 }
+#--------------------------------------
 #creating elastic ip
+#--------------------------------------
 resource "aws_eip" "eip" {
   vpc = true
 
@@ -153,146 +171,3 @@ resource "aws_eip" "eip" {
     project = var.project
   }
 }
-/*
-#creating key
-resource "aws_key_pair" "mykey" {
-  key_name   = "zomato"
-  public_key = file("mykey.pub")
-
-  tags = {
-    Name = var.project
-    project = var.project
-  }
-}
-# SG for bastion
-resource "aws_security_group" "bastion" {
-  name        = "bastion"
-  description = "Allow 22 from all"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = var.project
-    project = var.project
-  }
-}
-# SG for front end
-resource "aws_security_group" "frontend" {
-  name        = "frontend"
-  description = "Allow 22 from bastion and 80 from all"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.bastion.id]
-  }
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = var.project
-    project = var.project
-  }
-}
-# SG for back end
-resource "aws_security_group" "backend" {
-  name        = "backend"
-  description = "Allow 22 and 3306 from bastion"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.bastion.id]
-  }
-  ingress {
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.bastion.id]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = var.project
-    project = var.project
-  }
-}
-#creating ec2 bastion
-resource "aws_instance" "bastion" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.mykey.id
-  vpc_security_group_ids = [aws_security_group.bastion.id]
-  subnet_id              = aws_subnet.public1.id
-
-  tags = {
-    Name = "bastion"
-    project = var.project
-  }
-}
-#creating ec2 frontend
-resource "aws_instance" "frontend" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.mykey.id
-  vpc_security_group_ids = [aws_security_group.frontend.id]
-  user_data              = file("frontend.sh")
-  subnet_id              = aws_subnet.public1.id
-
-  tags = {
-    Name = "frontend"
-    project = var.project
-  }
-}
-#creating ec2 backend
-resource "aws_instance" "backend" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.mykey.id
-  vpc_security_group_ids = [aws_security_group.backend.id]
-  user_data              = file("backend.sh")
-  subnet_id              = aws_subnet.private1.id
-
-  tags = {
-    Name = "backend"
-    project = var.project
-  }
-}*/
